e62a708d84a3de7766e0e5986cc373c472ae9fe3
강혁준 <gurwns232@naver.com>
2022-09-30 23:20:45 +0900

    separate script

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper')
+const header = wrapper.querySelector('header')
+const content = wrapper.querySelector('.content')
+
+function onDrag({movementX, movementY}) {
+    let getStyle = window.getComputedStyle(wrapper)
+    let left = parseInt(getStyle.left)
+    let top = parseInt(getStyle.top)
+
+    wrapper.style.left = `${left + movementX}px`
+    wrapper.style.top = `${top + movementY}px`
+}
+
+content.addEventListener('mousedown', () => {
+    content.classList.add('active')
+    content.addEventListener('mousemove', onDrag)
+})
+
+document.addEventListener('mouseup', () => {
+    content.classList.remove('active')
+    content.removeEventListener('mousemove', onDrag)
+})
